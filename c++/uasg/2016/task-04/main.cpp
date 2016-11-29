c20662b85caddec8d85264c3aa49c69364d24b36
Aleksandar Savev <savevjr@gmail.com>
2016-11-29 23:50:43 +0200

    Add task-04 to uasg

---
+++ c++/uasg/2016/task-04/main.cpp
@@ -0,0 +1,84 @@
+#include <istream>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int M = 0;
+int N = 0;
+
+float** loadMatrix(const char* filename) {
+    ifstream in(filename);
+
+    if (!in) {
+        cout << "Cannot open file.\n";
+        return NULL;
+    }
+    
+    in >> M;
+    in >> N;
+
+    float** matrix = new float*[N];
+    for(int i = 0; i < N; i++)
+        matrix[i] = new float[N];
+    
+    for (int y = 0; y < M; y++)
+        for (int x = 0; x < N; x++)
+            in >> matrix[y][x];
+
+    in.close();
+    return matrix;
+}
+
+void printMatrix(float** matrix) {
+    for (int y = 0; y < M; y++) {
+        for (int x = 0; x < N; x++)
+            cout << setw(7) << setprecision(3) << matrix[y][x] << ' ';
+        cout << endl;
+    }
+}
+
+float** buildMatrix(float** source) {
+    float** matrix = new float*[N];
+    for(int i = 0; i < N; i++)
+        matrix[i] = new float[N];
+    
+    for (int y = 0; y < N; y++) {
+        for (int x = 0; x < N; x++) {
+            matrix[y][x] = 0;
+            for (int i = 0; i < N; i++) {
+                matrix[y][x] += source[y][i] * source[i][x];
+            }
+        }
+    }
+    return matrix;
+}
+
+int countPositiveNumbers(float** matrix) {
+    int counter = 0;
+    for (int y = 0; y < M; y++)
+        for (int x = 0; x < N; x++)
+            if (matrix[y][x] > 0)
+                counter++;
+    return counter;
+}
+
+int main() {
+    float ** input = loadMatrix("input.txt");
+    cout << "Input (matrix A):" << endl;
+    printMatrix(input);
+
+    if (M != N) {
+        cout << "The matrix must be a square to perform the operation." << endl;
+        return 0;
+    }
+
+    float ** result = buildMatrix(input);
+    cout << endl << "Scalar product matrix (matrix B):" << endl;
+    printMatrix(result);
+    cout << endl << "Positive numbers in matrix A: " << countPositiveNumbers(input);
+    cout << endl << "Positive numbers in matrix B: " << countPositiveNumbers(result) << endl;
+    return 0;
+}
