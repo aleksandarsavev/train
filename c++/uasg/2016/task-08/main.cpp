a3ecdaa96163f7e113c7dc450b2f3b4f87ca828d
Aleksandar <savevjr@gmail.com>
2016-11-29 10:39:25 +0200

    Update main.cpp

--- c++/uasg/2016/task-08/main.cpp
+++ c++/uasg/2016/task-08/main.cpp
@@ -5,8 +5,9 @@
 
 using namespace std;
 
+int N = 0;
+
 float** loadMatrix(const char* filename) {
-    int N;
     ifstream in(filename);
 
     if (!in) {
@@ -16,12 +17,10 @@ float** loadMatrix(const char* filename) {
     
     in >> N;
 
-    // Alocate memory for the input matrix
     float** matrix = new float*[N];
     for(int i = 0; i < N; i++)
         matrix[i] = new float[N];
     
-    // Read matrix from input file stream
     for (int y = 0; y < N; y++)
         for (int x = 0; x < N; x++)
             in >> matrix[x][y];
@@ -30,7 +29,32 @@ float** loadMatrix(const char* filename) {
     return matrix;
 }
 
+void printMatrix(float** matrix) {
+    for (int j = 0; j < N; j++) {
+        for (int i = 0; i < N; i++)
+            cout << matrix[i][j] << ' ';
+        cout << endl;
+    }
+}
+
+float** buildMatrix(float** source) {
+    float** matrix = new float*[N];
+    for(int i = 0; i < N; i++)
+        matrix[i] = new float[N];
+    
+    for (int j = 0; j < N; j++)
+        for (int i = 0; i < N; i++)
+            if ((i + j) % 2 == 0)
+                matrix[i][j] = source[i][j] + source[i][j];
+            else
+                matrix[i][j] = source[i][j] - source[i][j];
+            
+    return matrix;
+}
+
 int main() {
-    float ** matrix = loadMatrix("input.txt");
-    cout << matrix[0][0];
+    float ** input = loadMatrix("input.txt");
+    printMatrix(input);
+    float ** result = buildMatrix(input);
+    printMatrix(result);
 }
