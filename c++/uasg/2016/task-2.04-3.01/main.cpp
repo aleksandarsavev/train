b4ee091ac044910f26386a33e24aeb21d594b4e0
Aleksandar Savev <savevjr@gmail.com>
2016-12-20 23:21:46 +0200

    Update task-2.04-3.01

--- c++/uasg/2016/task-2.04-3.01/main.cpp
+++ c++/uasg/2016/task-2.04-3.01/main.cpp
@@ -1,4 +1,4 @@
-// task-2.13-2.08
+// task-2.04-3.01
 #include <fstream>
 #include <iostream>
 
@@ -7,7 +7,8 @@ using namespace std;
 int M = 0;
 int N = 0;
 
-float** X,Y;
+float **X;
+float **Y;
 
 float** loadMatrix(const char* filename) {
     ifstream in(filename);
@@ -62,10 +63,35 @@ int main() {
     cout << "The matrix X is a " << (M == N ? "square" : "rectangle") << "."  << '\n';
  
     if (M == N) {
-        
+        int positives = 0;
+        float sump = 0;
+        int negatives = 0;
+        float sumn = 0;
+
+        for (int y = 0; y < M; y++) {
+            for (int x = y + 1; x < N; x++) {
+                if(X[y][x] > 0) {
+                    positives++;
+                    sump += X[y][x];
+                } else if (X[y][x] < 0){
+                    negatives++;
+                    sumn += X[y][x];
+                }
+            }
+        }
+
+        cout << "Average of the positive numbers over the main diagonal: " << sump / positives << '\n';
+        cout << "Average of the negative numbers over the main diagonal: " << sumn / negatives << '\n';
     }
 
-    cout << "X - Z = \n";
+    cout << "X + Y = \n";
+    for (int y = 0; y < M; y++) {
+        for (int x = 0; x < N; x++) {
+            cout << X[y][x] + Y[y][x] << "  ";
+        }
+        cout << '\n';
+    }
+    cout << "X - Y = \n";
     for (int y = 0; y < M; y++) {
         for (int x = 0; x < N; x++) {
             cout << X[y][x] - Y[y][x] << "  ";
