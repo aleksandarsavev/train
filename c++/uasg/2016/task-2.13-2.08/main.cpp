58ca9799035be4cf27fd9069800247d791568647
Aleksandar Savev <savevjr@gmail.com>
2016-12-20 10:55:29 +0200

    Update task-2.13-2.08

--- c++/uasg/2016/task-2.13-2.08/main.cpp
+++ c++/uasg/2016/task-2.13-2.08/main.cpp
@@ -68,5 +68,77 @@ int main() {
         cout << "Dot product (K = " << K << "): " << result << endl;
     }
 
+    // Sums rows
+    for (int y = 0; y < M; y++) {
+        int result = 0;
+        for (int x = 0; x < N; x++) {
+            result += input[y][x];
+        }
+        cout << "Sum of row [" << y << "] = " << result << '\n'; 
+    }
+
+    // Sums columns
+    for (int x = 0; x < N; x++) {
+        int result = 0;
+        for (int y = 0; y < M; y++) {
+            result += input[y][x];
+        }
+        cout << "Sum of column [" << x << "] = " << result << '\n'; 
+    }
+
+    // Product rows
+    for (int y = 0; y < M; y++) {
+        int result = 1;
+        for (int x = 0; x < N; x++) {
+            result *= input[y][x];
+        }
+        cout << "Product of row [" << y << "] = " << result << '\n'; 
+    }
+
+    // Product columns
+    for (int x = 0; x < N; x++) {
+        int result = 1;
+        for (int y = 0; y < M; y++) {
+            result *= input[y][x];
+        }
+        cout << "Product of column [" << x << "] = " << result << '\n'; 
+    }
+
+    // Minimum rows
+    for (int y = 0; y < M; y++) {
+        int result = input[y][0];
+        for (int x = 1; x < N; x++) {
+            result = result < input[y][x] ? result : input[y][x];
+        }
+        cout << "Minimum of row [" << y << "] = " << result << '\n'; 
+    }
+
+    // Minimum columns
+    for (int x = 0; x < N; x++) {
+        int result = input[0][x];
+        for (int y = 0; y < M; y++) {
+            result = result < input[y][x] ? result : input[y][x];
+        }
+        cout << "Minimum of column [" << x << "] = " << result << '\n'; 
+    }
+
+    // Maximum rows
+    for (int y = 0; y < M; y++) {
+        int result = input[y][0];
+        for (int x = 1; x < N; x++) {
+            result = result > input[y][x] ? result : input[y][x];
+        }
+        cout << "Maximum of row [" << y << "] = " << result << '\n'; 
+    }
+
+    // Maximum columns
+    for (int x = 0; x < N; x++) {
+        int result = input[0][x];
+        for (int y = 0; y < M; y++) {
+            result = result > input[y][x] ? result : input[y][x];
+        }
+        cout << "Maximum of column [" << x << "] = " << result << '\n'; 
+    }
+
     return 0;
 }
