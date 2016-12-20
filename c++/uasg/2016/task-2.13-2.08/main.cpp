b923754bfb90f51b6972ca790f4da29210df656c
Aleksandar Savev <savevjr@gmail.com>
2016-12-20 07:42:31 +0200

    Add task-2.13-2.08

---
+++ c++/uasg/2016/task-2.13-2.08/main.cpp
@@ -0,0 +1,72 @@
+// task-2.13-2.08
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int M = 0;
+int N = 0;
+
+float** loadMatrix(const char* filename) {
+    ifstream in(filename);
+
+    if (!in) {
+        cout << "Cannot open file.\n";
+        return NULL;
+    }
+    
+    in >> M;
+    in >> N;
+
+    float** matrix = new float*[M];
+    for(int i = 0; i < M; i++)
+        matrix[i] = new float[N];
+    
+    for (int y = 0; y < M; y++)
+        for (int x = 0; x < N; x++)
+            in >> matrix[y][x];
+
+    in.close();
+    return matrix;
+}
+
+void printMatrix(float** matrix) {
+    for (int y = 0; y < M; y++) {
+        for (int x = 0; x < N; x++) {
+            cout << matrix[y][x] << "  ";
+        }
+        cout << '\n';
+    }
+}
+
+float getMaximum(float** matrix, int row) {
+    float max = matrix[0][0];
+    for (int y = 0; y < M; y++)
+        for (int x = 0; x < N; x++)
+            max = (matrix[y][x] < max) ? max : matrix[y][x];
+    return max;
+}
+
+int main() {
+    float ** input = loadMatrix("input.txt");
+    cout << "Input (matrix A):" << '\n';
+    printMatrix(input);
+
+    cout << "The matrix is a " << (M == N ? "square" : "rectangle") << "."  << '\n';
+ 
+    if (M == N) {
+        int K = -1;
+        while (K < 0){
+            cout << "Enter a column K [1-" << M << "]: ";
+            cin >> K;
+            if (K < 1 || K > M) K = -1;
+        }
+
+        float result = 0;
+        for (int i = 0; i < M; i++)
+            result += input[i][i] * input[i][K - 1];
+        cout << "Dot product (K = " << K << "): " << result << endl;
+    }
+
+    return 0;
+}
