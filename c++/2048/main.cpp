152941abc9b5e255bbb5a28e4084e4f3e5dd0d54
Aleksandar Savev <savevjr@gmail.com>
2017-01-09 17:30:04 +0200

    Add 2048

---
+++ c++/2048/main.cpp
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "2048\n";
+    
+}
+
+
---
+++ c++/2048/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+const int AUTO_MODE = 1;
+const int MANUAL_MODE = 2;
+
+int width, height;
+int ** m;
+
+void start();
+
+#endif
