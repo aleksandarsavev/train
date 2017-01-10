60b8870e3a07a3df2a209102b5d031e41e329734
Aleksandar Savev <savevjr@gmail.com>
2017-01-10 17:41:23 +0200

    Update 2048

--- c++/2048/main.cpp
+++ c++/2048/main.cpp
@@ -1,11 +1,35 @@
 #include "main.h"
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
-    cout << "2048\n";
-    
+    log("2048");
+    setup(m);
+    print(m);
+}
+
+void setup(struct matrix *m) {
+    m->width = 8;
+    m->height = 8;
+    m->m = new int*[m->height];
+    for (int i = 0; i < m->height; i++) {
+        m->m[i] = new int[m->width];
+    }
+}
+
+void print(struct matrix *m) {
+    for (int i = 0; i < m->height; i++) {
+        for (int j = 0; j < m->width; j++) {
+	   cout << m->m[i][j] << setw(10) << ' ';
+	}
+	cout << '\n';
+    }
+}
+
+void inline log(const char *msg) {
+    cout << msg << '\n';
 }
 
 
--- c++/2048/main.h
+++ c++/2048/main.h
@@ -1,13 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 const int AUTO_MODE = 1;
 const int MANUAL_MODE = 2;
 
+struct matrix {
+    int width, height;
+    int **m;
+} *m = new matrix;
+
 int width, height;
-int ** m;
 
+void setup(struct matrix *m);
 void start();
+void move(int *dir);
 
+void print(struct matrix *m);
+void inline log(const char * msg);
 #endif
