69b0033ffe32014044173fca6fdf504844b178db
Aleksandar Savev <savevjr@gmail.com>
2017-09-04 14:21:14 +0300

    Add endpoints for primes

--- python/primes/main.py
+++ python/primes/main.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python
 from flask import Flask
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
+api = Api(app)
+
+
+
+MINERS = []
+
+class Miners(Resource):
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('name')
+
+    def get(self):
+        return MINERS
+
+    def post(self):
+        args = self.parser.parse_args()
+        print(args)
+        return 'Hi'
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return 'Hello, World!'
+
+api.add_resource(Miners, '/miners')
 
 if __name__ == '__main__':
     app.run(debug=True)
